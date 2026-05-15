c00b9ed11a37c05f0f8ae448bae2ada87c1b485a
David Morris <david.morris@me.com>
2026-05-15 11:32:08 +0100

    Add docs

--- lib/node/src/index.d.ts
+++ lib/node/src/index.d.ts
@@ -1,23 +1,21 @@
 /**
  * A minimal library for pivoting data by 1-n dimensions.
- * 
+ *
  * The {@link pivot} function slices data and dices data by one or more {@link Dimension dimensions}, returning a {@link Cube}, which is a nested array as deep as the number of dimensions passed into the {@link pivot} operstion.
- * 
+ *
  * Simple {@link Dimension dimensions} can be created by mapping a set of values using the {@link property} function or a custom mapping function.
- * 
+ *
  * Once a {@link Cube} is created, the {@link query} function can be used to perform query operations on the subset of the source data in each cell.
- * 
+ *
  * @module
  */
 import { Func, Predicate } from "@davidwcmorris/types";
-
 /**
  * A dimension is a set of Predicates used to partition data.
  * @typeParam TElement The type of the source data that the {@link Dimension} was created for.
  * @category Type declarations
  */
 export type Dimension<TElement> = readonly Predicate<readonly [TElement]>[];
-
 /**
  * A Cube is an n-dimensional data structure.
  * Each {@link Dimension} adds one array level.
@@ -26,16 +24,13 @@ export type Dimension<TElement> = readonly Predicate<readonly [TElement]>[];
  * @category Type declarations
  */
 export type Cube<TElement, TDimensions extends readonly unknown[]> = TDimensions extends readonly [unknown, ...infer TRest] ? Cube<TElement[], TRest> : TElement[];
-
 /**
  * Creates a predicate function for use in the {@link dimension} function to create a {@link Dimension} matching properties.
  * @typeParam TElement The type of the source data that will be evaluated by the generated predicate. This is generally not inferred, so is recommended to provide.
  * @param key The property in the source data to base this predicate on.
  * @category Cube building
  */
-export const property = <TElement>(key: keyof TElement): Func<Predicate<readonly [TElement]>, readonly [TElement[keyof TElement]]> =>
-	value => element => element[key] === value;
-
+export declare const property: <TElement>(key: keyof TElement) => Func<Predicate<readonly [TElement]>, readonly [TElement[keyof TElement]]>;
 /**
  * Slices and dices a set of elements based on the criteria defined in one or more dimensions.
  * @typeParam TElement The type of the source data that will be evaluated by the generated predicates. This is inferred from the elements passed into the function.
@@ -44,10 +39,7 @@ export const property = <TElement>(key: keyof TElement): Func<Predicate<readonly
  * @param dimensions The dimensions to slice and dice the data by.
  * @returns Returns an Cube, which is an n-dimensional array mirroring the number of dimensions plus the set of elements.
  */
-export function pivot<TElement, TDimensions extends readonly [Dimension<TElement>, ...Dimension<TElement>[]]>(elements: readonly TElement[], ...[first, second, ...others]: TDimensions): Cube<TElement, TDimensions> {
-	return (second ? slice(elements, first).map(vector => pivot(vector, second, ...others)) : slice(elements, first)) as Cube<TElement, TDimensions>;
-}
-
+export declare function pivot<TElement, TDimensions extends readonly [Dimension<TElement>, ...Dimension<TElement>[]]>(elements: readonly TElement[], ...[first, second, ...others]: TDimensions): Cube<TElement, TDimensions>;
 /**
  * Queries data from a {@link Cube} using a selector function to transform the elements in each cell into a result.
  * @typeParam TCell The type of the data within the dimensions of the {@link Cube}.
@@ -56,18 +48,14 @@ export function pivot<TElement, TDimensions extends readonly [Dimension<TElement
  * @param selector A callback function to create a result from each cell of the {@link Cube}.
  * @category Cube query
  */
-export const query = <TCell extends readonly unknown[], TResult>(cube: TCell[][], selector: Func<TResult, readonly [TCell]>): TResult[][] =>
-	cube.map(slice => slice.map(selector));
-
+export declare const query: <TCell extends readonly unknown[], TResult>(cube: TCell[][], selector: Func<TResult, readonly [TCell]>) => TResult[][];
 /**
 * Create a callback function to pass into {@link query} that sums numerical values derived by the selector function.
 * @typeParam TElement The type of the data within the cube that will be passed into the selector.
 * @param selector A callback function to derive a numerical value for each object in the source data.
 * @category Cube query
 */
-export const sum = <TElement>(selector: Func<number, readonly [TElement]>): Func<number, readonly [TElement[]]> =>
-	vector => vector.reduce((accumulator, element) => accumulator + selector(element), 0);
-
+export declare const sum: <TElement>(selector: Func<number, readonly [TElement]>) => Func<number, readonly [TElement[]]>;
 /**
  * Create a callback function to pass into {@link query} that averages numerical values derived by the selector function.
  * @typeParam TElement The type of the data within the cube that will be passed into the selector.
@@ -75,22 +63,5 @@ export const sum = <TElement>(selector: Func<number, readonly [TElement]>): Func
  * @returns Returns the average given the selector; note that for empty cells this will be NaN
  * @category Cube query
  */
-export const average = <TElement>(selector: Func<number, readonly [TElement]>): Func<number, readonly [TElement[]]> =>
-	vector => sum(selector)(vector) / vector.length;
-
-// slices the data by one dimension
-const slice = <TElement>(elements: readonly TElement[], dimension: Dimension<TElement>): TElement[][] =>
-	dimension.map(predicate => fastFilter(elements, predicate));
-
-// fast alternative to Array.prototype.filter
-function fastFilter<T>(array: readonly T[], predicate: Predicate<readonly [T]>): T[] {
-	const result: T[] = [];
-
-	for (let index = 0; index < array.length; ++index) {
-		if (predicate(array[index])) {
-			result.push(array[index]);
-		}
-	}
-
-	return result;
-}
+export declare const average: <TElement>(selector: Func<number, readonly [TElement]>) => Func<number, readonly [TElement[]]>;
+//# sourceMappingURL=index.d.ts.map
